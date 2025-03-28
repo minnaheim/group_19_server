e71693ee0a0220967651483fc0f00b3c0f46bde0
Ivan Isaenko <ivan.isaenko@uzh.ch>
2025-03-28 16:01:17 +0100

    Add Movie entity, resolves #289

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Movie.java
@@ -0,0 +1,122 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Internal Movie Representation
+ * This class composes the internal representation of the movie and defines how
+ * the movie is stored in the database.
+ * Every variable will be mapped into a database field with the @Column
+ * annotation
+ * - nullable = false -> this cannot be left empty
+ * - unique = true -> this value must be unqiue across the database -> composes
+ * the primary key
+ */
+
+@Entity
+@Table(name = "MOVIE")
+public class Movie implements Serializable {
+
+//   private static final long serialVersionUID = 1L;
+
+  // movie attributes
+
+//   if we generate id by ourselves
+// @Id
+//   @GeneratedValue
+
+// if we use id from tmdb
+  @Column(nullable=false, unique=true)
+  private int id;
+
+  @Column
+  private String title;
+
+  @Column
+  private String posterUrl;
+
+  @Column
+  private String details;
+
+  @Column
+  private String genre;
+
+  @Column
+  private String director;
+
+  @Column
+  private List<String> actors;
+
+  @Column
+  private String trailerURL;
+
+//   getters and setters
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getPosterUrl() {
+    return posterUrl;
+  }
+
+  public void setPosterUrl(String posterUrl) {
+    this.posterUrl = posterUrl;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+
+  public void setDetails(String details) {
+    this.details = details;
+  }
+
+  public String getGenre() {
+    return genre;
+  }
+
+  public void setGenre(String genre) {
+    this.genre = genre;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+  public List<String> getActors() {
+    return actors;
+  }
+
+  public void setActors(List<String> actors) {
+    this.actors = actors;
+  }
+
+  public String getTrailerURL() {
+    return trailerURL;
+  }
+
+  public void setTrailerURL(String trailerURL) {
+    this.trailerURL = trailerURL;
+  }
+}
