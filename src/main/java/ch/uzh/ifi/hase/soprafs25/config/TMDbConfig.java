3810df3122ee083f2b6572117d1a154886e02370
AnabelNigsch <anabel.nigsch@uzh.ch>
2025-04-17 23:08:08 +0200

    Local works try secret 1

--- src/main/java/ch/uzh/ifi/hase/soprafs25/config/TMDbConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/config/TMDbConfig.java
@@ -1,19 +1,34 @@
 package ch.uzh.ifi.hase.soprafs25.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource(value = "file:./local.properties", ignoreResourceNotFound = true)
-public class TMDbConfig {
+public class TMDbConfig implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(TMDbConfig.class);
 
     @Value("${TMDB_API_TOKEN:${tmdb.api.key:}}")
-    //@Value("${tmdb.api.key:}")
     private String apiKey;
 
     @Value("${tmdb.api.base-url:https://api.themoviedb.org/3}")
     private String baseUrl;
+
+    @Override
+    public void run(String... args) {
+        String envToken = System.getenv("TMDB_API_TOKEN");
+        if (envToken != null && !envToken.isBlank()) {
+            logger.info("TMDB_API_TOKEN is available as an environment variable.");
+        } else {
+            logger.info("TMDB_API_TOKEN is not set as an environment variable. Using local properties.");
+        }
+        logger.info("TMDb Base URL is set to: {}", baseUrl);
+    }
 
     public String getApiKey() {
         return apiKey;
