a4d476fc613afcd1e263ed109f883a91e44d7bda
AnabelNigsch <anabel.nigsch@uzh.ch>
2025-04-17 17:08:45 +0200

    using GIT secret für TMDb api token 1

--- src/main/java/ch/uzh/ifi/hase/soprafs25/config/TMDbConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/config/TMDbConfig.java
@@ -4,29 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-/* @Configuration
+@Configuration
 @PropertySource(value = "file:./local.properties", ignoreResourceNotFound = true)
 public class TMDbConfig {
 
-    @Value("${tmdb.api.key:}")
-    private String apiKey;
-
-    @Value("${tmdb.api.base-url:https://api.themoviedb.org/3}")
-    private String baseUrl;
-
-    public String getApiKey() {
-        return apiKey;
-    }
-
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-}
-*/
-@Configuration
-public class TMDbConfig {
-
     @Value("${TMDB_API_TOKEN:${tmdb.api.key:}}")
+    //    @Value("${tmdb.api.key:}")
     private String apiKey;
 
     @Value("${tmdb.api.base-url:https://api.themoviedb.org/3}")
@@ -40,5 +23,3 @@ public class TMDbConfig {
         return baseUrl;
     }
 }
-
-
