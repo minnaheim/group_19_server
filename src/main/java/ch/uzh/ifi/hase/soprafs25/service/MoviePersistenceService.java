03f92fb6f91837a149615dc6fd9f60d1d4c57b37
AnabelNigsch <anabel.nigsch@uzh.ch>
2025-05-10 10:25:08 +0200

    https://github.com/minnaheim/group_19_server/issues/546

--- src/main/java/ch/uzh/ifi/hase/soprafs25/service/MoviePersistenceService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/service/MoviePersistenceService.java
@@ -6,13 +6,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import ch.uzh.ifi.hase.soprafs25.entity.Movie;
 import ch.uzh.ifi.hase.soprafs25.repository.MovieRepository;
-import java.util.ArrayList;
+import ch.uzh.ifi.hase.soprafs25.entity.Movie;
+import ch.uzh.ifi.hase.soprafs25.entity.User;
+import ch.uzh.ifi.hase.soprafs25.repository.MovieRepository;
+import ch.uzh.ifi.hase.soprafs25.repository.UserRepository;
+
 
 @Service
 public class MoviePersistenceService {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    @Autowired
+    private MovieService movieService; //
 
     @Transactional
     public Movie saveOrGet(Movie movie) {
@@ -21,11 +28,25 @@ public class MoviePersistenceService {
         if (existing != null) {
             return existing;
         }
+
+        /*
+        ANI CHANGE so that only complete movies are saved to our dbs
         // Initialize collections for new movie to avoid null element collections
         if (movie.getGenres() == null) movie.setGenres(new ArrayList<>());
         if (movie.getActors() == null) movie.setActors(new ArrayList<>());
         if (movie.getDirectors() == null) movie.setDirectors(new ArrayList<>());
         if (movie.getSpokenlanguages() == null) movie.setSpokenlanguages(new ArrayList<>());
         return movieRepository.save(movie);
+        */
+        long movieId = movie.getMovieId();
+        Movie savedMovie = getAndSaveMovieById(movieId);
+        return savedMovie;
+    }
+    /**
+     * Helper method to get and save a movie by ID
+     */
+    private Movie getAndSaveMovieById(Long movieId) {
+        Movie movie = movieService.getMovieById(movieId);
+        return movieService.saveMovie(movie);
     }
 }
