e73b22ed562a1348066b8c7ed3b580f6833d128e
Ivan Isaenko <ivan.isaenko@uzh.ch>
2025-05-19 12:27:02 +0200

    Fix authorixation errors

--- src/main/java/ch/uzh/ifi/hase/soprafs25/utils/AuthorizationUtil.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs25/utils/AuthorizationUtil.java
@@ -1,10 +1,13 @@
 package ch.uzh.ifi.hase.soprafs25.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 public class AuthorizationUtil {
     public static String extractToken(String authorizationHeader) {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             return authorizationHeader.substring(7); // Remove "Bearer " prefix
         }
-        throw new IllegalArgumentException("Invalid Authorization header");
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
     }
 }
